679186b45538a12be51fd6b592e9fa666b18feaf
Day-Ham <daehankimm17@gmail.com>
2026-02-19 10:42:07 +0800

    Database Made

--- GameStore.API/obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524338a47fac06876a14e8bb468d197fb04d9bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203da15eefc2ee0b221a66ed3ab03dad4c9246d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
